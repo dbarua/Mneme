002cc82ed8a80ac0a2abaf5ae0f3b36c21fe0447
dbarua <debjanee.barua@gmail.com>
2012-06-25 14:14:37 +1000

    Session implemented in some part

--- Mneme_v2/mneme.sh
+++ Mneme_v2/mneme.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
-export PYTHONPATH=/home/jbu/src/jbu-personis/Src:/home/jbu/src/jbu-personis/Src/Util
-python main_new.py --appconfig app.jbu.conf --globalconfig global.jbu.conf
+#export PYTHONPATH=/home/jbu/src/jbu-personis/Src:/home/jbu/src/jbu-personis/Src/Util
+#python main_new.py --appconfig app.jbu.conf --globalconfig global.jbu.conf
 
-#export PYTHONPATH=/home/chai/llum/llum/Personis/Src:/home/chai/llum/llum/Personis/Src/Util
-#python main_new.py
+export PYTHONPATH=/home/chai/llum/llum/Personis/Src:/home/chai/llum/llum/Personis/Src/Util
+python main_new.py
 
