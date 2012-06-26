66a2759de306d732f458cc7dfe1d23e67299c39d
dbarua <debjanee.barua@gmail.com>
2012-06-26 14:43:53 +1000

    mneme.sh added new personis

--- Mneme_v2/mneme.sh
+++ Mneme_v2/mneme.sh
@@ -3,6 +3,7 @@
 #export PYTHONPATH=/home/jbu/src/jbu-personis/Src:/home/jbu/src/jbu-personis/Src/Util
 #python main_new.py --appconfig app.jbu.conf --globalconfig global.jbu.conf
 
-export PYTHONPATH=/home/chai/llum/llum/Personis/Src:/home/chai/llum/llum/Personis/Src/Util
+#export PYTHONPATH=/home/chai/llum/llum/Personis/Src:/home/chai/llum/llum/Personis/Src/Util
+export PYTHONPATH=/home/chai/personis/server/Src:/home/chai/personis/server/Src/Utils
 python main_new.py
 
